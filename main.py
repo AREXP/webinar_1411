c38e87abca75be9d525f3acee54a2a76cb6b1e8a
AREXP <arexp19@gmail.com>
2020-11-14 10:51:42 +0300

    add count

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import sys
 
 
 def main():
+    count = 0
     for line in sys.stdin:
         print(line)
+        count += 1
+    print('-' * 20)
+    print('{} were read'.format(count))
 
 
 if __name__ == '__main__':
